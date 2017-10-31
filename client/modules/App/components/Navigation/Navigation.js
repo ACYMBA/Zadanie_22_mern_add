efd0c2a02675995210eb5cbfef66d9d67a745443
albert <albert.cymbaluk@gmail.com>
2017-10-31 16:05:19 +0100

    navigation

--- client/modules/App/components/Navigation/Navigation.js
+++ client/modules/App/components/Navigation/Navigation.js
@@ -1,64 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { browserHistory } from 'react-router';
+import styles from './Navigation.css';
+import { Link } from 'react-router';
 
-function() {
-	var container, button, menu, links;
+//const navOptions = ['home', 'posts', 'about'];
 
-	container = document.getElementById( 'site-navigation' );
-	if ( ! container )
-		return;
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //selectedPage: 'home',
+    };
+  }
 
-	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button )
-		return;
+  /*handleNavChange(option) {
+    this.setState({
+      selectedPage: option,
+    });
+    browserHistory.push(`/${option}`);
+  }*/
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+  render() {
+    return (
+      <div className={styles.navigation}>
+        <ul>
+                <li>
+                    <Link to="/home" ><FormattedMessage id="home" /></Link>
+                </li>
+                <li>
+                    <Link to="/" ><FormattedMessage id="posts" /></Link>
+                </li>
+                
+                <li>
+                    <Link to="/about" ><FormattedMessage id="about" /></Link>
+                </li>
 
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
+        </ul>
+      </div>
+    );
+  }
+}
 
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
-		menu.className += ' nav-menu';
-
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) )
-			container.className = container.className.replace( ' toggled', '' );
-		else
-			container.className += ' toggled';
-	};
-
-	// Get all the link elements within the menu. Return if none are found.
-	links = menu.getElementsByTagName( 'a' );
-	
-	if ( 0 === links.length )
-		return;
-
-	// Each time a menu link is focused or blurred call the function toggle_focus
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].onfocus = toggle_focus;
-		links[i].onblur = toggle_focus;
-	};
-
-	function toggle_focus() {
-		
-		// Skipping the actual link itself as this can be styled with :focus
-		var current = this.parentElement;
-		var parents = []
-	 
-		// Build an array of the parent elements of the focused link until we hit the top level .nav-menu ul
-		while ( -1 === current.className.indexOf( 'nav-menu' ) ) {
-			parents.unshift( current );
-			current = current.parentElement;
-		}
-	 
-		// For each element in parents[] toggle the class .focus
-		for ( i = 0, len = parents.length; i < len; i++ ) {
-			if ( -1 !== parents[i].className.indexOf( 'focus' ) )
-				parents[i].className = parents[i].className.replace( ' focus', '' );
-			else
-				parents[i].className += ' focus';
-		}
-	}
-} )();
+export default Navigation;
